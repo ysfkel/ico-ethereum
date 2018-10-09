4515066ef3c6a7c2a90f256382430e4c3ee273df
YUSUF <yusufkel@gmail.com>
2018-10-10 00:02:56 +0400

    update

--- truffle.js
+++ truffle.js
@@ -13,6 +13,22 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks:{
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id:'*'
+    },
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id:'*'
+    },
+  },
+  solc:{
+    optimizer:{
+      enabled: true,
+      runs: 200
+    }
+  }
 };
